fe24ee4ee91a5aa88b08b203cbbdae2bde455e2c
firatfurkan <furkanfiraat@gmail.com>
2025-06-17 10:54:40 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'akilli-sepet-v2';
+const CACHE_NAME = 'akilli-sepet-cache-v1';
 const urlsToCache = [
   './',
   './index.html',
@@ -9,29 +9,12 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
-});
-
-// Push Notification Listener
-self.addEventListener('push', event => {
-  const data = event.data.json();
-  const options = {
-    body: data.body,
-    icon: 'icon-192.png',
-    badge: 'icon-192.png'
-  };
-  event.waitUntil(
-    self.registration.showNotification(data.title, options)
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
